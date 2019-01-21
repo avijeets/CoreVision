0180dc0f4ad635e347b9226ec7058abe6ad134e1
avijeet <a@avijeets.com>
2019-01-21 14:24:30 -0500

    analyzes photo data

--- CoreVision/Controller/CameraViewController.swift
+++ CoreVision/Controller/CameraViewController.swift
@@ -16,6 +16,7 @@ class CameraViewController: UIViewController {
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
     var flashControl: Flash = .off
+    var photoData: Data?
     
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
     @IBOutlet weak var cameraView: UIView!
@@ -60,5 +61,66 @@ class CameraViewController: UIViewController {
             debugPrint(error)
         }
     }
+    
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            
+            // unknown object found
+            if classification.confidence < 0.5 {
+                print(classification.identifier)
+                let unknownObjectMessage = "I'm not sure what this is. Please try again."
+                self.identificationLabel.text = unknownObjectMessage
+                self.confidenceLabel.text = ""
+                break
+            }
+            
+            // found object with confidence
+            else {
+                let identification = classification.identifier
+                let confidence = Int(classification.confidence * 100)
+                self.identificationLabel.text = identification
+                self.confidenceLabel.text = "CONFIDENCE: \(confidence)%"
+                let completeSentence = "This looks like a \(identification) and I'm \(confidence) percent sure."
+                break
+            }
+        }
+    }
+    
+    @IBAction func flashPressed(_ sender: Any) {
+        switch flashControl {
+        case .on:
+            flashButton.setTitle("Flash Off", for: .normal)
+            flashControl = .off
+        case .off:
+            flashButton.setTitle("Flash On", for: .normal)
+            flashControl = .on
+        }
+    }
 }
 
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        }
+        else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+                
+            }
+            catch {
+                debugPrint(error)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+    }
+}
